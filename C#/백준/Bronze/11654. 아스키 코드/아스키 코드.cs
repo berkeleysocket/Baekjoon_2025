f6ac88da22d8bc2c4f0d81c726e9233e25d975ab
hwani165 <seungyung.kang@gmail.com>
2025-07-16 10:46:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 72 ms, Memory: 5448 KB -BaekjoonHub

---
+++ C#/백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)Console.ReadLine()[0]);
+        }
+    }
+}
